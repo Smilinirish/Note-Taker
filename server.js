17c82dbdbad055f23f19a76006ac93f4564c7a69
Alexander VanOsdol <alex.m.vanosdol@me.com>
2021-12-01 11:11:31 -0500

    added dependanceis and server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+//build standard express app
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
+
+// ROUTS 
+ //HTML ROUTES
+// home Route 
+    // send index.html
+// notes route
+    //send notes.html
+// API Routs
